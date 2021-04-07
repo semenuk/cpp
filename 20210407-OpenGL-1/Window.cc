caaf75b0e537f72f970ee8516b9ca7eb39906972
unyuu <unyuu@tenshi.nekomimi.nya>
2021-04-07 13:23:46 +0500

    Реализован конструктор Window

--- 20210407-OpenGL-1/Window.cc
+++ 20210407-OpenGL-1/Window.cc
@@ -5,7 +5,32 @@
  *      Author: unyuu
  */
 
+#include <string>
+#include <stdexcept>
 #include "Window.h"
 
+Window::Window(int width, int height)
+: _width(width), _height(height)
+{
+	using namespace std::literals;
 
+	_window = std::shared_ptr<SDL_Window>(
+			SDL_CreateWindow(
+					"OpenGL Window",
+					DEFAULT_X, DEFAULT_Y,
+					width, height,
+					SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL),
+			SDL_DestroyWindow);
+	if (_window == nullptr)
+		throw std::runtime_error(
+				"Не удалось создать окно: "s + SDL_GetError());
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	_gl_context = std::shared_ptr<void>(
+			SDL_GL_CreateContext(_window.get()),
+			SDL_GL_DeleteContext);
+	if (_gl_context == nullptr)
+		throw std::runtime_error(
+				"Не удалось создать контекст OpenGL: "s + SDL_GetError());
+}
--- 20210407-OpenGL-1/Window.h
+++ 20210407-OpenGL-1/Window.h
@@ -8,10 +8,30 @@
 #ifndef WINDOW_H_
 #define WINDOW_H_
 
+#include <memory>
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
 class Window
 {
 public:
+	constexpr int DEFAULT_WIDTH = 1920;
+	constexpr int DEFAULT_HEIGHT = 1080;
+	constexpr int DEFAULT_X = SDL_WINDOWPOS_CENTERED_DISPLAY(1);
+	constexpr int DEFAULT_Y = SDL_WINDOWPOS_CENTERED_DISPLAY(1);
+
+protected:
+	std::shared_ptr<SDL_Window> _window; // Окно
+	std::shared_ptr<void> _gl_context; // Контекст OpenGL
+	int _width, _height; // Ширина и высота окна
+
+public:
+	Window(int width = DEFAULT_WIDTH, int height = DEFAULT_HEIGHT);
 	virtual ~Window() = default;
+
+	int height() const { return _height; }
+	int width() const  { return _width;  }
 };
 
 #endif /* WINDOW_H_ */
