a7c9c88eb18fad5c4b1928dd4ff04012b3528258
unyuu <unyuu@tenshi.nekomimi.nya>
2021-04-07 14:48:13 +0500

    Разноцветный четырёхугольник

--- 20210407-OpenGL-1/DemoWindow.cc
+++ 20210407-OpenGL-1/DemoWindow.cc
@@ -16,22 +16,23 @@ DemoWindow::DemoWindow(int width, int height)
 void DemoWindow::setup_gl()
 {
 	glClearColor(0.2f, 0.4f, 0.7f, 1.0f);
+	glPointSize(16.0f);
+	glLineWidth(12.0f);
 }
 
 void DemoWindow::render()
 {
 	glClear(GL_COLOR_BUFFER_BIT); // Очистка
 
-	glBegin(GL_TRIANGLES);
+	glBegin(GL_QUADS);
 
-	/* glVertex???
-	 * 		   ^ количество координат (2, 3 или 4)
-	 * 		    ^ тип значений координат (d - double, f - float,
-	 * 		    	i - int, s - short)
-	 * 		     ^ v - значит, массив.
-	 */
-	glVertex2d(0.0, 0.8);
+	glColor3d(1.0, 1.0, 0.0);
+	glVertex2d(-0.8, 0.8);
+	glColor3d(0.0, 1.0, 0.0);
+	glVertex2d(0.8, 0.8);
+	glColor3d(1.0, 0.0, 1.0);
 	glVertex2d(0.8, -0.8);
+	glColor3d(0.0, 1.0, 1.0);
 	glVertex2d(-0.8, -0.8);
 
 	glEnd();
