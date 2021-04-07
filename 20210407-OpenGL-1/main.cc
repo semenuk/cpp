4721f71cc68ff69f5b5cfc717e56f28c516c7708
unyuu <unyuu@tenshi.nekomimi.nya>
2021-04-07 12:41:36 +0500

    Начало работы с проектом

---
+++ 20210407-OpenGL-1/main.cc
@@ -0,0 +1,27 @@
+/*
+ * main.cc
+ *
+ *  Created on: 7 апр. 2021 г.
+ *      Author: unyuu
+ */
+
+/*
+		Библиотеки:
+
+		Для Windows и MinGW:
+		mingw32
+		SDL2main
+		SDL2
+		SDL2_image
+		opengl32
+		glu32
+
+		Для UNIX-подобных:
+		SDL2
+		SDL2_image
+		GL
+		GLU
+ */
+
+
+
